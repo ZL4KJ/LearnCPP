a0c100109816c8636d47373986df4d3f31c4824d
Kimberly Jansen <kim@xunil.co.nz>
2023-09-29 12:39:57 +1300

    Messing with Git in Visual Studio

---
+++ WinRTConsoleApp/main.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::Web::Syndication;
+using namespace std;
+
+int main()
+{
+    winrt::init_apartment();
+    wcout << "Setting up RSS Feed Uri\n";
+    Uri rssFeedUri{ L"https://blogs.windows.com/feed" };
+    wcout << "Setting up Syndication Client\n";
+    SyndicationClient syndicationClient;
+
+    SyndicationFeed syndicationFeed = syndicationClient.RetrieveFeedAsync(rssFeedUri).get();
+
+    for (const SyndicationItem syndicationItem : syndicationFeed.Items())
+    {
+        //...
+        winrt::hstring titleAsHstring = syndicationItem.Title().Text();
+
+        // Omitted: there's a little bit of extra work here to remove the trademark symbol from the title text.
+
+        std::wcout << titleAsHstring.c_str() << std::endl;
+    }
+
+
+    //Uri uri(L"http://aka.ms/cppwinrt");
+    //printf("Hello, %ls!\n", uri.AbsoluteUri().c_str());
+}
---
+++ WinRTConsoleApp/pch.h
@@ -0,0 +1,8 @@
+﻿// pch.h
+#pragma once
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Web.Syndication.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <iostream>
+#include <winrt/base.h>
+#pragma comment(lib, "windowsapp")
