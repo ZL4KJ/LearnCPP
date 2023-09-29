2d76bceb020a9d9363524ae02b8ea13761936702
Kimberly Jansen <kim@xunil.co.nz>
2023-09-29 13:19:32 +1300

    Added some comments

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -5,5 +5,21 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // This is a single-line comment.
+
+    /* 
+        This is a 
+        multi-line
+        comment
+    */
+
+    int x = 5; // decleration
+    int y = 6;
+    int sum = x + y; // declare sum and make the value stored the value of x + y
+
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "It's really windy today\n";
+
+    std::cout << x << " + " << y << " = " << sum; // write the value of sum to the console
+    return 0;
 }
